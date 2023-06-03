7114b0d201f342099b713784d8434d253ef962b1
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-06-03 15:11:46 +0530

    Fix configs

--- iap/build.gradle.kts
+++ iap/build.gradle.kts
@@ -22,12 +22,12 @@ android {
     }
 
     kotlin {
-        jvmToolchain(8)
+        jvmToolchain(17)
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     publishing {
