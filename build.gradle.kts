cb1b28c593ca3c437affc40cca5740a9bfad2f17
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2024-01-12 22:02:48 +0530

    Release 1.6.0!

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
     val kotlinVersion = "1.9.22"
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("com.android.tools.build:gradle:8.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
