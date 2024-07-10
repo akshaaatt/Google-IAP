3afc2ab3d14883efb80e5a7894cb70d6fd57c0f2
Akshat Tiwari <tiwariakshat03@gmail.com>
2024-07-11 01:38:49 +0530

    Bump release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.limurse.iapsample"
         minSdk = 21
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
--- iap/build.gradle.kts
+++ iap/build.gradle.kts
@@ -54,7 +54,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.limurse"
             artifactId = "Google-IAP"
-            version = "1.6.0"
+            version = "1.7.0"
 
             afterEvaluate {
                 from(components["release"])
